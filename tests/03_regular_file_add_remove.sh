7000a3a6909294aed8c3ad794edbdddc3261c45c
Alberto <alberto.ruiz@codethink.co.uk>
2011-10-03 15:29:29 +0100

    Improvements to the test framework and documentation

--- tests/03_regular_file_add_remove.sh
+++ tests/03_regular_file_add_remove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TEST_ID="03"
-TEST_NAME="Symlink add/remove"
+TEST_ID="01"
+TEST_NAME="Regular file add remove"
 
 CREATE=`pwd`/$1
 DEPLOY=`pwd`/$2
@@ -14,17 +14,17 @@ TEST_TOOLS=$3
 # This test checks that normal files content and metadata are 
 
 function setup {
-	ln -s /foo $ORIGIN/remove && \
-	ln -s /bar $TARGET/add && \
+	touch $ORIGIN/remove && \
+	echo 1 > $TARGET/add && \
 	chown -h :cdrom $TARGET/add
 }
 
 # check_same_mtime FILE_A FILE_B
 
 function check_results {
-	test   -L $ORIGIN/add    && \
-	test ! -L $ORIGIN/remove && \
-	check_symlink     $ORIGIN/add "/bar"      && \
+	test   -f $ORIGIN/add    && \
+	test ! -f $ORIGIN/remove && \
+	check_content     $ORIGIN/add "1"         && \
 	check_same_mtime  $ORIGIN/add $TARGET/add && \
 	check_same_uidgid $ORIGIN/add $TARGET/add
 }
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-if [ ! -f $TESTLIB ]
+if [ ! -f run_tests.sh ]
+then
+	echo "Test suite needs to be run from the tests directory" 1>&2
+	exit 1
+fi
+
+if [ ! -f test_lib.sh ]
 then
 	echo "Could not find test_lib.sh" 1>&2
+	exit 1
 fi
+
+ALLTESTSDIR=`pwd`
 
 for i in [0-9][0-9]*
 do
-	./$i $1 $2
+	cd $ALLTESTSDIR
+	./$i ../tbdiff-create ../tbdiff-deploy
 	if [ $? -ne 0 ]
 	then
+		echo "Test program $i failed" 1>&2
+		cd $ALLTESTSDIR
 		exit 1
 	fi
 done
