ece2c8f162a0f810ccaff946c216def18cc47f2f
Alberto Ruiz <alberto.ruiz@codethink.co.uk>
2011-10-04 02:08:12 +0100

    Enhance dir add remove test

--- tests/07_dir_add_remove.sh
+++ tests/07_dir_add_remove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TEST_ID="05"
-TEST_NAME="Symlink add/remove"
+TEST_ID="07"
+TEST_NAME="Directory add remove"
 
 CREATE=`pwd`/$1
 DEPLOY=`pwd`/$2
@@ -12,17 +12,20 @@ TEST_TOOLS=$3
 ############# Test specific code ############
 
 function setup {
-	mkdir $ORIGIN/remove && \
-	mkdir $TARGET/add && \
+	mkdir -p $ORIGIN/remove/1/2/3/4 && \
+	mkdir -p $TARGET/add/4/3/2/1 && \
 	chown -h :cdrom $TARGET/add
+	chown -h :cdrom $TARGET/add/4/3/2/1
 }
 
 function check_results {
-	test   -d $ORIGIN/add    && \
+	test   -d $ORIGIN/add/4/3/2/1    && \
 	test ! -d $ORIGIN/remove && \
 	check_same_mtime  $ORIGIN/add $TARGET/add && \
 	check_same_mode   $ORIGIN/add $TARGET/add && \
-	check_same_uidgid $ORIGIN/add $TARGET/add
+	check_same_uidgid $ORIGIN/add $TARGET/add && \
+	check_same_mode   $ORIGIN/add $TARGET/add && \
+	check_same_uidgid $ORIGIN/add/4/3/2/1 $TARGET/add/4/3/2/1
 }
 
 #############################################
