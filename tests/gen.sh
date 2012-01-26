807c3eebe1de06f8a81fd64ede0249a6339f19fe
Danny Abukalam <dannyabukalam@codethink.co.uk>
2012-01-26 17:23:27 +0000

    Corrupted git repo

---
+++ tests/gen.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# This script generates seven folders with incremental
+# file type additions. This setup allows for the creation
+# of patches for cross-platform testing of tbdiff. These patches
+# are then commited and recreated and compared on different
+# platforms to check compatibility between different systems
+# including endianness and architecture.
+
+. ./test_lib.sh
+
+labelmeta() {
+    touch -ht 197001010100.42 $1
+    chmod 607 $1
+}
+
+cleanup() {
+    for ((i=0; i<8; i++)); do
+        rm -rf ${nums[$i]}
+    done
+}
+
+if [ ! -f run_tests.sh ]
+then
+    echo "Test needs to be run from the tests directory" 1>&2
+    exit 1
+fi
+
+if [ ! $(id -u) -eq 0 ]
+then
+    echo "Root."
+    exit 1
+fi
+
+if [ -d PATCHES ]
+then
+    echo -n "Overwrite Existing Patches? "
+    read input
+    input=`echo $input | tr '[:upper:]' '[:lower:]'`
+    case "$input" in
+        [Yy]|'') ;;
+        [Nn]*) echo "Aborted."; exit 1;;
+        *) echo "??";;
+    esac
+fi
+
+rm -rf PATCHES
+
+mkdir -p zero
+mkdir -p PATCHES
+for ((i=1; i<8; i++)); do
+    mkdir -p ${nums[$i]}
+    cd ${nums[$i]}
+    insertfiles $i
+    for ((j=1; j<=$i; j++)); do
+        labelmeta ${files[$j]}
+    done
+    cd ..
+    tbdiff-create PATCHES/patch${nums[$i]}.tbd ${nums[$(($i - 1))]} ${nums[$i]}
+done
+
+cleanup
+
+exit 0
--- tests/test_lib.sh
+++ tests/test_lib.sh
@@ -6,6 +6,36 @@ IMGFILE=$TESTDIR/tbdiff.img
 ORIGIN=$TESTDIR/orig
 TARGET=$TESTDIR/target
 
+nums=( zero one two three four five six seven )
+files=( null a.txt b.txt dirdir dirdir/fifo dirdir/symlinkb dirdir/chardev \
+        dirdir/blockdev )
+
+TOPDIR=`pwd`
+
+insertfiles() {
+    if [ $1 -ge 1 ]; then
+        echo 1 > a.txt
+    fi  
+    if [ $1 -ge 2 ]; then 
+        cat $TOPDIR/lipsum.txt > b.txt
+    fi  
+    if [ $1 -ge 3 ]; then 
+        mkdir -p dirdir
+    fi  
+    if [ $1 -ge 4 ]; then 
+        mkfifo dirdir/fifo
+    fi  
+    if [ $1 -ge 5 ]; then 
+        ln -s ../b.txt dirdir/symlinkb
+    fi  
+    if [ $1 -ge 6 ]; then 
+        mknod dirdir/chardev c `stat /dev/null -c '%t %T'`
+    fi  
+    if [ $1 -ge 7 ]; then 
+        mknod dirdir/blockdev b `stat /dev/null -c '%t %T'`
+    fi  
+}
+
 # check_same_mtime FILE_A FILE_B
 check_same_mtime () {
 	test $(stat -c %Y $1) = $(stat -c %Y $2)
